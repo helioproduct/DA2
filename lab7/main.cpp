ff502c25faf7e672062870994f0604c5be4ccbc7
Nikolay Popov <helioproduct.sync@gmail.com>
2023-11-18 15:25:01 +0300

    solution added

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,23 +1,67 @@
 #include <iostream>
+#include <stack>
 #include <vector>
+
+using namespace std;
+
+int maxRectangleArea(vector<vector<int> >& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    vector<vector<int> > height(n, vector<int>(m, 0));
+    int maxArea = 0;
+
+    // Построение матрицы высот
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (matrix[i][j] == 0) {
+                height[i][j] = (i == 0) ? 1 : height[i - 1][j] + 1;
+            }
+        }
+    }
+
+    // Поиск максимальной площади прямоугольника в каждой строке
+    for (int i = 0; i < n; ++i) {
+        stack<int> indices;
+        int j = 0;
+        while (j < m) {
+            if (indices.empty() || height[i][indices.top()] <= height[i][j]) {
+                indices.push(j++);
+            } else {
+                int tp = indices.top();
+                indices.pop();
+                int area = height[i][tp] *
+                           (indices.empty() ? j : j - indices.top() - 1);
+                maxArea = max(maxArea, area);
+            }
+        }
+
+        while (!indices.empty()) {
+            int tp = indices.top();
+            indices.pop();
+            int area =
+                height[i][tp] * (indices.empty() ? m : m - indices.top() - 1);
+            maxArea = max(maxArea, area);
+        }
+    }
+
+    return maxArea;
+}
 
 int main() {
     int n, m;
-    std::cin >> n >> m;
+    cin >> n >> m;
+    vector<vector<int> > matrix(n, vector<int>(m));
 
-    std::vector<std::vector<int> > data(n, std::vector<int>(m));
-    std::string row;
-    for (int i = 0; i < n; i++) {
-        std::cin >> row;
-        for (int j = 0; j < row.size(); j++) {
-            // data[i][j] = row[j] - '0';
+    // Считывание матрицы
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            char ch;
+            cin >> ch;
+            matrix[i][j] = ch - '0';
         }
     }
 
-    for (std::vector<int> vec : data) {
-        for (int number : vec) {
-            std::cout << number << " ";
-        }
-        std::cout << std::endl;
-    }
+    cout << maxRectangleArea(matrix) << endl;
+
+    return 0;
 }
